fa8867259001715a692c3965e645dd265f6ff31c
Ifthe16 <habiburrahmanif975@gmail.com>
2024-10-22 02:10:36 +0600

    added css styles in app css file and made app file dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,21 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Device name="Laptop" price="55"></Device>
+      <Device name="mobile" price="17"></Device>
+      <Device name="watch" price="3"></Device>
       <Person></Person>
-      <Person></Person>
-      <Person></Person>
-      <Person></Person>
+      <Student grade="7" score="99"></Student>
       <Student></Student>
+      <Student></Student>
+      <Developer></Developer>
     </>
   )
+}
+
+function Device(props) {
+  // console.log(props)
+  return <h2>This device: {props.name} price: {props.price}</h2>
 }
 
 function Person() {
@@ -23,12 +31,35 @@ function Person() {
   return <h3>I am a {person.name} with age: {age + money}</h3>
 }
 
-function Student() {
+const { grade, score } = { grade: '7', score: '99' };
+function Student(props) {
+  console.log(props);
   return (
-    <div>
+    <div className='student'>
       <h3>This is a student</h3>
       <p>Name:</p>
       <p>age:</p>
+    </div>
+  )
+}
+
+function Developer() {
+  const developerStyle = {
+    margin: '20px',
+    padding: '20px',
+    border: '2px solid purple',
+    borderRadius: '20px'
+  }
+  return (
+    /*   <div style={{
+          margin: '20px',
+          padding: '20px',
+          border: '2px solid purple',
+          borderRadius: '20px'
+        }}> */
+    <div style={developerStyle}>
+      <h5>Devo devo</h5>
+      <p>Coding: </p>
     </div>
   )
 }
